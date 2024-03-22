6c4fe98f15a4d609ab505d050de8fc2d21891082
Jonathan Sönnerup <nooxet@gmail.com>
2024-03-22 20:14:22 +0100

    Reads the first line from a text file

--- src/ned.c
+++ src/ned.c
@@ -24,8 +24,8 @@ typedef struct
 
 typedef struct
 {
-    int rows;   // window size
-    int cols;
+    int winRows;   // window size
+    int winCols;
     int cx;     // cursor pos
     int cy;
     edRow_s row;
@@ -34,6 +34,9 @@ typedef struct
 
 
 static edConfig_s edConfig;
+
+static FILE *logFile = NULL;
+#define LOG(format, ...) fprintf(logFile, format, __VA_ARGS__)
 
 void edMoveCursor(int key)
 {
@@ -45,11 +48,11 @@ void edMoveCursor(int key)
             break;
         case ARROW_DOWN:
             edConfig.cy++;
-            if (edConfig.cy >= edConfig.rows - 1) edConfig.cy = edConfig.rows - 1;
+            if (edConfig.cy >= edConfig.winRows- 1) edConfig.cy = edConfig.winRows- 1;
             break;
         case ARROW_RIGHT:
             edConfig.cx++;
-            if (edConfig.cx >= edConfig.cols - 1) edConfig.cx = edConfig.cols - 1;
+            if (edConfig.cx >= edConfig.winCols - 1) edConfig.cx = edConfig.winCols - 1;
             break;
         case ARROW_LEFT:
             edConfig.cx--;
@@ -59,7 +62,7 @@ void edMoveCursor(int key)
             edConfig.cx = 0;
             break;
         case END:
-            edConfig.cx = edConfig.cols - 1;
+            edConfig.cx = edConfig.winCols - 1;
             break;
     }
 }
@@ -83,7 +86,7 @@ void edProcessKey()
         case PAGE_UP:
         case PAGE_DOWN:
             {
-                int times = edConfig.rows;
+                int times = edConfig.winRows;
                 while (times--) edMoveCursor(key == PAGE_UP ? ARROW_UP : ARROW_DOWN);
             }
             break;
@@ -103,19 +106,20 @@ void edProcessKey()
 
 void edDrawRows(astring *frame)
 {
-    for (int y = 0; y < edConfig.rows; y++)
+    for (int y = 0; y < edConfig.winRows; y++)
     {
         if (y < edConfig.numRows)
         {
-            int len = edConfig.row.size;
+            // limit text size to the window width
+            int len = edConfig.row.size > edConfig.winCols ? edConfig.winCols : edConfig.row.size;
             astringAppend(frame, edConfig.row.string, len);
         }
-        else if (y == edConfig.rows / 3)
+        else if (y == edConfig.winRows/ 3)
         {
             char welcome[128] = { 0 };
             int welcomeLen = snprintf(welcome, sizeof(welcome),
                     "ned, the blazingly fast text editor -- version %s", NED_VERSION);
-            int padding = (edConfig.cols - welcomeLen) / 2;
+            int padding = (edConfig.winCols - welcomeLen) / 2;
 
             if (padding)
             {
@@ -133,7 +137,7 @@ void edDrawRows(astring *frame)
 
         // Erase line by line as we print new text
         astringAppend(frame, DISPLAY_ERASE_LINE_CMD, DISPLAY_ERASE_LINE_LEN);
-        if (y < edConfig.rows - 1)
+        if (y < edConfig.winRows- 1)
         {
             // Don't print a newline on the last line, otherwise it's empty (no tilde)
             astringAppend(frame, "\r\n", 2);
@@ -163,16 +167,28 @@ void edRefreshScreen()
     astringFree(&frame);
 }
 
-void edOpen()
+void edOpen(const char *filename)
 {
-    char *line = "Hello, ned!";
-    ssize_t lineLen = strlen(line);
+    FILE *inputFile = fopen(filename, "r");
+    if (!inputFile) errExit("Failed to open file: %s", filename);
 
-    edConfig.row.size = lineLen;
-    edConfig.row.string = strdup(line);
-    if (edConfig.row.string == NULL) errExit("Failed to allocate memory for string");
+    char *line = NULL;
+    size_t bufferSize = 0;
+    ssize_t lineLen = getline(&line, &bufferSize, inputFile);
+    if (lineLen != -1)
+    {
+        // remove newline char(s) if present and terminate string
+        while (lineLen > 0 && (line[lineLen - 1] == '\n' || line[lineLen - 1] == '\r')) lineLen--;
+        line[lineLen - 1] = '\0';
 
-    edConfig.numRows = 1;
+        edConfig.row.size = lineLen;
+        // TODO(noxet): free this memory later, in edClose?
+        edConfig.row.string = strdup(line);
+        edConfig.numRows = 1;
+    }
+    
+    free(line);
+    fclose(inputFile);
 }
 
 void edInit()
@@ -182,19 +198,24 @@ void edInit()
     edConfig.numRows = 0;
 
     // TODO(noxet): Handle window resize event
-    if (termGetWindowSize(&edConfig.rows, &edConfig.cols) == -1) errExit("Failed to get window size");
+    if (termGetWindowSize(&edConfig.winRows, &edConfig.winCols) == -1) errExit("Failed to get window size");
 
 
-    printf("window size, rows: %d, cols: %d\n", edConfig.rows, edConfig.cols);
+    printf("window size, rows: %d, cols: %d\n", edConfig.winRows, edConfig.winCols);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    logFile = fopen("ned.log", "w");
+
     if (termEnableRawMode() == -1) errExit("Failed to set raw mode");
     if (termSetupSignals() == -1) errExit("Failed to set up signal handler");
 
     edInit();
-    edOpen();
+    if (argc >= 2)
+    {
+        edOpen(argv[1]);
+    }
 
 
     // disable stdout buffering
